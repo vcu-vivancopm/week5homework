6c2abaa11eee98b8d7b7c1e0f87b5e44acf3d53c
vcu-vivancopm <78439442+vcu-vivancopm@users.noreply.github.com>
2021-03-08 21:33:26 -0500

    Update homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -6,6 +6,7 @@ def find_greatest_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the largest number in the list.
     """
+    max(incoming_list)
     pass
 
 
@@ -14,6 +15,7 @@ def find_least_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the smallest/least number in the list.
     """
+    min(incoming_list)
     pass
 
 
